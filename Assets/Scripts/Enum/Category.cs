cf542ec8b240c7f58aa42b41b8a9eb0de410465e
Rabia <113474565+Rabia94@users.noreply.github.com>
2022-09-14 10:03:07 +0300

    Yeni kelimeler eklendi

---
+++ Assets/Scripts/Enum/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+[Serializable]
+public enum Category
+{
+    Fiil = 1,
+    Böcek = 4,
+    Hayvan = 3,
+    İsim = 2,
+    Bitki = 5,
+    El,
+    Body,
+
+}
--- Assets/Scripts/ScriptableObject/Word.cs
+++ Assets/Scripts/ScriptableObject/Word.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "Word")]
 public class Word : ScriptableObject
 {
-
+    public Category Category;
+    public string Label;
+    public Sprite Image;
+    public AudioClip Audio;
 }
