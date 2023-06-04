76b0de16e5a94c38cfa44ba8a5b5a8ce2d1a5446
SkyWalker2506 <42008533+SkyWalker2506@users.noreply.github.com>
2023-06-04 18:28:48 +0300

    no message

---
+++ Assets/PatientInfoManager.cs
@@ -0,0 +1,13 @@
+using TMPro;
+using UnityEngine;
+
+public class PatientInfoManager : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _name;
+    [SerializeField] private TMP_Dropdown _gender;
+    [SerializeField] private TMP_InputField _day;
+    [SerializeField] private TMP_InputField _month;
+    [SerializeField] private TMP_InputField _year;
+    [SerializeField] private TMP_Dropdown _education;
+    [SerializeField] private TMP_InputField _employment;
+}
